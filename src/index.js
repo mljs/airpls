77d931fdba532a6c8c07f0e4b247be9a3ac4bbe0
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2017-12-12 10:23:13 -0500

    (work in progress) start to use cuthillMckee

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
-import {CHO} from 'ml-matrix';
-import {SparseMatrix} from 'ml-sparse-matrix';
+import Cholesky from 'cholesky-solve';
+import cuthillMckee from 'cuthill-mckee';
 
 function airPLS(data, options = {}) {
     console.time('prepro');
     let {
         maxIterations = 100,
         lambda = 100,
-        factorCriterion = 0.001
+        factorCriterion = 0.001,
+        weights
     } = options;
 
     var nbPoints = data.length;
     var stopCriterion = factorCriterion * data.reduce((sum, e) => Math.abs(e) + sum, 0);
 
-    console.time('diff')
-    var deltaMatrix = getDeltaMatrix(nbPoints);
-    console.timeEnd('diff')
-    var wMatrix = SparseMatrix.identity(nbPoints, nbPoints);
+    if (!weights) {
+        weights = new Array(nbPoints).fill(1);
+    }
+
     console.time('cov');
-    var covMatrix = deltaMatrix.transpose().mmul(deltaMatrix).mul(lambda);
+    var {A, P} = getDeltaMatrix(nbPoints, lambda, weights); //choice another name
     console.timeEnd('cov');
-    var sumNegDifferences = Number.MAX_SAFE_INTEGER;
+    return;
+    console.time('decomposition');
+    let cho = Cholesky.prepare(A, nbPoints, P);
+    console.timeEnd('decomposition');
     console.timeEnd('prepro');
-    return
+    return;
+    var sumNegDifferences = Number.MAX_SAFE_INTEGER;
     for (var iteration = 0; (iteration < maxIterations && Math.abs(sumNegDifferences) > stopCriterion); iteration++) {
         console.time('iteration: ' + iteration);
-        let rightHandSide = covMatrix.clone();
+
         let vector = data.map((e, i) => {
             rightHandSide[i][i] += wMatrix[i][i];
             return [e * wMatrix[i][i]]
         });
         
-        let cho = new CHO(rightHandSide);
         var baseline = cho.solve(vector);
 
         sumNegDifferences = 0;
@@ -64,12 +68,38 @@ function airPLS(data, options = {}) {
     };
 }
 
-function getDeltaMatrix(nbPoints) {
-    let matrix = SparseMatrix.eye(nbPoints - 1, nbPoints);
-    for (let i = 0, l = nbPoints-1; i < l; i++) {
-        matrix.set(i, i + 1, -1);
+function getDeltaMatrix(nbPoints, lambda, weights) {
+    var matrix = [];
+    for (var i = 0, last = nbPoints - 1; i < last; i++) {
+        matrix.push([i, i, lambda * 2 + weights[i]]);
+        matrix.push([i, i + 1, -lambda]);
+        matrix.push([i + 1, i, -lambda]);
     }
-    return matrix;
+    matrix[0][2] = lambda + weights[0];
+    matrix.push([last, last, lambda + weights[last]]);
+    console.log(matrix);
+    console.log(nbPoints);
+    return {A: matrix, P: cuthillMckee(matrix, nbPoints)};
+}
+
+function cholUpdate(matrix, x) {
+    let r, s, c;
+    let n = x.length;
+    let L = matrix.clone();
+    for (let i = 0; i < n; i++) {
+        if (x[i] === 0) continue;
+        let li = L[i][i];
+        let xi = x[i]
+        r = Math.sqrt(li * li + xi * xi);
+        c = r / li;
+        s = xi / li;
+        L[i][i] = r;
+        for (let j = i; j < n; i++) {
+            L[j][i] = (L[j][i] + s * x[j]) / c;
+            x[j] = c * x[j] - s * L[j][i];
+        }
+    }
+    return L;
 }
 
 export {airPLS as default};
